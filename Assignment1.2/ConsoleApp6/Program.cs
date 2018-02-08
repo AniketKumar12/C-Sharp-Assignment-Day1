54d1b4d492c1228fdff3131b68f1f38db9839068
soheshdoshi <doshisohesh@gmail.com>
2018-02-08 11:05:53 +0530

    C-Sharp-Assignment-Day1

---
+++ Assignment1.2/ConsoleApp6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static string[] words = new string[] { "Boat", "House", "Cat", "River", "Cupboard" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plurals:-");
+            foreach (var w in words)
+                Console.WriteLine(w + "s");
+            
+
+            Console.WriteLine("\nReplaced:-");
+            words[1] = "home";
+            foreach (var w in words)
+                Console.WriteLine(w);
+
+            Console.WriteLine("\nnew word:-");
+            List<string> word = words.ToList();
+            word.Add("Dog");
+            words = word.ToArray();
+            foreach (var w in words)
+                Console.WriteLine(w);
+        
+
+            Console.WriteLine("\nWords:-");
+            var wordlen = Array.FindAll(words, r => r.Length == 7);
+            foreach (var w in wordlen)
+                Console.WriteLine(w);
+
+            Console.WriteLine("\n3rd Word Position:-{0}",words[2]);
+           
+            
+
+            Console.WriteLine("\nAscending Order:-");
+            var ascending = words.OrderBy(r => r);
+            foreach (var w in ascending)
+                Console.WriteLine(w);
+            
+            Console.WriteLine("\nReverse Order:-");
+            var reverse = words.Reverse();
+            foreach (var w in reverse)
+                Console.WriteLine(w);
+
+            /*  for (int i = 0; i < words.Count; i++)
+              {
+                  if(words[i].EndsWith('y'))
+                  {
+                      int a = words[i].LastIndexOf('y');
+
+                      words[i]=substring + "ies";
+                  }
+                  else
+                  {
+                      words[i] = words[i] + "es";
+                  }
+
+                  Console.WriteLine(words[i]);
+              }*/
+
+        }
+    }
+}
---
+++ Assignment1/Assignment/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment
+{
+   public  class Program
+    {
+        string ProdutName;
+        string ProdutPrice;
+        int ProdutQuantity;
+        string ProdutType;
+
+        public int Quantity
+        {
+            get { return ProdutQuantity; }
+            set { ProdutQuantity = value; }
+        }
+
+        public string Name
+        {
+            get { return ProdutName; }
+            set { ProdutName = value; }
+        }
+        public string Price
+        {
+            get { return ProdutPrice; }
+            set { ProdutPrice = value; }
+        }
+        public string Type
+        {
+            get { return ProdutType; }
+            set { ProdutType = value; }
+        }
+        public override string ToString()
+        {
+            return "ProductName:-" + Name + " " +"ProductPrice:-"+ Price+" "+"ProdctQuantity:-"+Quantity+" "+"ProductType:-"+Type;
+        }
+       static List<Program> newlist = new List<Program>();
+
+
+        static void Main(string[] args)
+        {
+            /* List<Product> productList = new List<Product>() {
+                 new Product(){ ProductName="lettuce", ProductPrice="10.5 RS",ProductQuantity=50,ProductType="Leafy green"},
+                 new Product(){ ProductName="cabbage", ProductPrice="20 RS",ProductQuantity=100,ProductType="Cruciferous"},
+                 new Product(){ ProductName="pumpkin", ProductPrice="30 RS",ProductQuantity=30,ProductType="Marrow"},
+                 new Product(){ ProductName="cauliflower", ProductPrice="10 RS",ProductQuantity=25,ProductType="Cruciferous"},
+                 new Product(){ ProductName="zucchini", ProductPrice="20.5 RS",ProductQuantity=50,ProductType="Marrow"},
+                 new Product(){ ProductName="yam", ProductPrice="30 RS",ProductQuantity=50,ProductType="Root"},
+                 new Product(){ ProductName="spinach", ProductPrice="10 RS",ProductQuantity=100,ProductType="Leafy green"},
+                 new Product(){ ProductName="broccoli", ProductPrice="20.2 RS",ProductQuantity=75,ProductType="Cruciferous"},
+                 new Product(){ ProductName="Garlic", ProductPrice="30 RS",ProductQuantity=20,ProductType="Leafy green"},
+                 new Product(){ ProductName="silverbeet", ProductPrice="10 RS",ProductQuantity=50,ProductType="Marrow"},
+             };
+             Console.WriteLine("total number of Product in Proct List:-{0}",productList.Count);
+             foreach (Product item in productList)
+                 Console.WriteLine(item);*/
+
+            /*Program obj = new Program();
+            string ProdutName1 = "abc";
+            string ProdutPrice1 = "12 Rs";
+            int ProdutQuantity1 = 12;
+            string ProdutType1 = "Xyz";
+            obj.ProdutName = ProdutName1;
+            obj.ProdutPrice = ProdutPrice1;
+            obj.ProdutQuantity = ProdutQuantity1;
+            obj.ProdutType = ProdutType1;
+            Program.newlist.Add(obj);*/
+            List<Program> newlist = new List<Program>() {
+                 new Program(){ Name="lettuce", Price="10.5 RS",Quantity=50,Type="Leafy green"},
+                 new Program(){ Name="cabbage", Price="20 RS",Quantity=100,Type="Cruciferous"},
+                 new Program(){ Name="pumpkin", Price="30 RS",Quantity=30,Type="Marrow"},
+                 new Program(){ Name="cauliflower", Price="10 RS",Quantity=25,Type="Cruciferous"},
+                 new Program(){ Name="zucchini", Price="20.5 RS",Quantity=50,Type="Marrow"},
+                 new Program(){ Name="yam", Price="30 RS",Quantity=50,Type="Root"},
+                 new Program(){ Name="spinach", Price="10 RS",Quantity=100,Type="Leafy green"},
+                 new Program(){ Name="broccoli", Price="20.2 RS",Quantity=75,Type="Cruciferous"},
+                 new Program(){ Name="Garlic", Price="30 RS",Quantity=20,Type="Leafy green"},
+                 new Program(){ Name="silverbeet", Price="10 RS",Quantity=50,Type="Marrow"}
+            };
+            Console.WriteLine("total number of Product in Proct List:-{0}", newlist.Count);
+            Console.WriteLine("After Adding Product");
+            newlist.Add(new Program() { Name = "Potato", Price = "10 RS", Quantity = 50, Type = "Root" } );
+            /*Program obj = new Program() { Name = "Potato", Price = "10 RS", Quantity = 50, Type = "Root"  };
+            Program.newlist.Add(obj);*/
+            Console.WriteLine("total number of Product in Proct List:-{0}", newlist.Count);
+            // Console.WriteLine("total number of Product in Proct List:-{0}", newlist.Count.GetType());
+            foreach (var item in newlist)
+                Console.WriteLine(item);
+            Console.WriteLine("------");
+            foreach (var item in newlist)
+            {
+                if (item.Type.Equals("Leafy green"))
+                    Console.WriteLine(item);
+            }
+            // newlist.Remove( new Program(){ Name = "Potato", Price = "10 RS", Quantity = 50, Type = "Root" });
+
+            var itemToRemove = newlist.Single(r => r.Name == "Garlic");
+            newlist.Remove(itemToRemove);
+            Console.WriteLine("total number of Product in Proct List:-{0}", newlist.Count);
+
+            foreach (var item in newlist)
+            {
+                if (item.Name.Equals("cabbage"))
+                {
+                    int a = item.Quantity + 50 ;
+                    item.Quantity = a;
+                    Console.WriteLine(item);
+                }
+                    
+            }
+
+          
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+   /* public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public int  ProductQuantity { get; set; }
+        public string ProductType { get; set; }
+
+    }*/
+}
