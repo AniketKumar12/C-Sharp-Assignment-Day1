9fdd2ecc113832088faf52000967a9d011ea1ff5
soheshdoshi <doshisohesh@gmail.com>
2018-02-09 16:03:21 +0530

    Day2Assignment Update

---
+++ Assignment2.1/ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 0.06f;
+            var y = 5.94f;
+            Console.WriteLine("Sum Of X and y{0}:-",x + y);
+
+            Console.WriteLine("Equals:-{0}",(x + y).Equals(6));
+
+            Console.WriteLine(Garter(x, y));
+             string Garter(float x1,float y1)
+            {
+                if (x1 > y1)
+                    return "X is Grater";
+                else
+                    return "Y is Grater";
+
+            }
+        }
+    }
+}
---
+++ Assignmnet2/Assignmnet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignmnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            IList<string> strList = new List<string>();
+            strList.Add("Tomato");
+            strList.Add("Potato");
+            strList.Add("ToMato");
+            strList.Add("Potato");
+            strList.Add("Red Tomato");
+            strList.Add("Tomato Catchup");
+            strList.Add("Grapes");
+            strList.Add("grapes");
+            strList.Add("potato");
+            for (int i = 0; i < strList.Count; i++)
+            {
+                if (strList[i].Equals("tomato", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    count++;
+                }
+                //if you are dealing with key then use Contains coz Contains search key in O(1).
+                if(strList[i].Equals("Potato"))
+                {
+                    Console.WriteLine("index:-{0}",i);
+                }
+            }
+            Console.WriteLine("Count:-{0}",count);
+       
+         }
+
+       
+
+        
+    
+    }
+}
